830c290b00e5ad880a557b08f826602162cdddb0
Michel Medeiros <michel.tds@gmail.com>
2018-11-24 19:19:55 -0200

    Criando estrutura inicial do projeto

---
+++ src/main/java/br/com/dbunit/dbunit/DbunitApplication.java
@@ -0,0 +1,12 @@
+package br.com.dbunit.dbunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbunitApplication.class, args);
+	}
+}
